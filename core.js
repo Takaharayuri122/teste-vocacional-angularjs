57e2714ffb5393eb299b51d105c8ed22ca9307ee
TakaharaYuri <yuricarvalho48@gmail.com>
2021-06-11 03:17:48 -0300

    Avaliação + Objeto de questões via JSON e resultado final do teste

--- core.js
+++ core.js
@@ -6,78 +6,72 @@ angular
   .module('app', ['ngDialog', 'ui.mask', 'ngMask'])
 
   .controller('MainController', function ($scope, $http, ngDialog, $sce) {
+
+    $scope.entity = [];
     console.log('MainController Load');
     $scope.selectedOptions = [];
     $scope.questionPosition = 0;
-    $scope.selectedOptionId = '';
-    $scope.questions = [
-      {
-        id: 0,
-        title: 'Na escola, você prefere assuntos ligados à:',
-        options: [
-          {
-            option: 'a',
-            text: 'Arte, esportes e atividades extracurriculares'
-          },
-          {
-            option: 'b',
-            text: 'Biologia e genética'
-          },
-          {
-            option: 'c',
-            text: 'Ciências humanas, idiomas'
-          },
-          {
-            option: 'd',
-            text: 'Ciências exatas'
-          },
-        ]
-      },
-      {
-        id: 1,
-        title: 'Na escola, você prefere assuntos ligados à:',
-        options: [
-          {
-            id: 'a',
-            text: 'Arte, esportes e atividades extracurriculares'
-          },
-          {
-            id: 'b',
-            text: 'Biologia e genética'
-          },
-          {
-            id: 'c',
-            text: 'Ciências humanas, idiomas'
-          },
-          {
-            id: 'd',
-            text: 'Ciências exatas'
-          },
-        ]
-      }
-    ];
+    $scope.reference = [];
+    $scope.questions = [];
+    $scope.screen = 'pontuation';
 
-    var _a = [];
-    var _b = [];
-    var _c = [];
-    var _d = [];
+    $http.get('questionario.json').then(response => {
+      $scope.questions = response.data;
+    })
+
+    $scope._a = [];
+    $scope._b = [];
+    $scope._c = [];
+    $scope._d = [];
 
     $scope.onPreviuosPage = function () {
       $scope.questionPosition--;
     }
     $scope.onNextPage = function () {
       $scope.questionPosition++;
+
+      if ($scope.questionPosition == 19) {
+        processQuestions();
+      }
+
+
     }
+
 
     $scope.onSelectOption = function (item, option, index) {
 
-      $scope.selectedOptionId = option.option;
-
-      $scope.selectedOptions[index] = {
+      $scope.entity[item.id] = {
         question: item,
-        option: option
+        option: option.id
       };
-
-      console.log($scope.selectedOptions);
     }
+
+
+    processQuestions = function () {
+      $scope.entity.map(item => {
+        if (item.option == 'a') $scope._a.push(item.option)
+        if (item.option == 'b') $scope._b.push(item.option)
+        if (item.option == 'c') $scope._c.push(item.option)
+        if (item.option == 'd') $scope._d.push(item.option)
+      });
+
+      console.log('_a', $scope._a);
+      console.log('_b', $scope._b);
+      console.log('_c', $scope._c);
+      console.log('_d', $scope._d);
+
+    }
+
+    $scope.nextScreen = function (screen) {
+      console.log('iefneo');
+      $scope.screen = screen;
+    }
+
+    $scope.getPontuation = function () {
+      var _arrays = [$scope._a, $scope._b, $scope._c, $scope._d];
+      _arrays.sort(function(a, b){return b.length - a.length});
+      $scope.screen = _arrays[0][0];
+    }
+
+
   });
